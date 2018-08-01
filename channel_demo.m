31ff8fccc6b020608aa985b9df4300f989ae74a7
Willy <willyhome85@gmail.com>
2018-08-01 20:22:26 +0800

    Comment for channel code update

--- channel_demo.m
+++ channel_demo.m
@@ -1,26 +1,41 @@
 %% channel parameter 
 close all;
 clear;
-% 910 GHz
+
+% set the default frequency 910 GHz
 freq = 910*10^6;
-% 10 dBm
+% set the default amplitude 10 dBm
+% and set an 1X100 random amplitude array from 5 to 20
 ampl = 10;
 ampl_ran = randi([5 20],1,100);
-%distance 
+
+% distance from 1 to 100 cm
 d = 1:100;
-% light speed
+% light speed in cm/s
 LS = 3 * 10^10;
 
+% time = distance / light speed
 t = d./LS;
 
+%% calculate for phase 
+% 1j for distinguish from variable to complex 
 phase = -2*1j*pi * freq * t;
 
+% constant channel and random channel with constant amplitude and random
+% amplitude, respectively
 h = ampl .* exp(phase);
 h_ran = ampl_ran .* exp(phase);
 
+%% Draw for the results
+
 figure();
+subplot(2,2,[1,2])
+plot(h);
+title("Static Raw Signal")
+
+subplot(2,2,[3,4])
 plot(h_ran);
-title("Raw Signal")
+title("Dynamic Raw Signal")
 
 figure();
 subplot(2,2,1)
@@ -38,4 +53,4 @@ title("\color[rgb]{0 .5 .5}Dynamic Amplitude");
 
 subplot(2,2,4)
 plot(angle(h_ran));
-title("\color[rgb]{0 .5 1}Phase with Unconstant Amplitude");
+title("\color[rgb]{0 .5 1}Phase with Dynamic Amplitude");
